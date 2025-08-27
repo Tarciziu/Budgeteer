b8c28615daf53d77bce7af78681155e6e050c43c
Tarciziu <christin99tarciziu@gmail.com>
2025-08-27 21:37:27 +0300

    Add CoreUI module
    - Created and added CoreUI module to the project
    
    Ticket: $13a
    
    Type: New Feature

---
+++ Modules/CoreUI/CoreUI/CoreUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreUIApp.swift
+//  CoreUI
+//
+//  Created by Tarciziu Gologan on 27.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoreUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
