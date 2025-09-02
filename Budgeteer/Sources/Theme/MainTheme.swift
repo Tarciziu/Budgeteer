ac63d6a8fb57c01d68676ea6d9718340d2895b05
Tarciziu <christin99tarciziu@gmail.com>
2025-09-02 22:17:29 +0300

    Add Typography catalog
    - Added a custom fonts catalog providing support for different fonts
    
    Ticket: $139
    
    Type: New Feature

---
+++ Budgeteer/Sources/Theme/MainTheme.swift
@@ -0,0 +1,50 @@
+//
+//  MainTheme.swift
+//  Budgeteer
+//
+//  Created by Tarciziu Gologan on 30.08.2025.
+//
+
+import SwiftUI
+import BTCoreUI
+
+// MARK: - Typography Definition
+
+extension Typography {
+  static let mainTypography = Typography(
+    title: Title(
+      title1: BTFont(fontName: "TimesNewRomanPS-BoldMT", size: 28, lineHeight: 34, textStyle: .title1),
+      title2: BTFont(fontName: "TimesNewRomanPS-BoldMT", size: 22, lineHeight: 28, textStyle: .title2),
+      title3: BTFont(fontName: "TimesNewRomanPS-BoldMT", size: 20, lineHeight: 25, textStyle: .title3),
+      headline: BTFont(fontName: "TimesNewRomanPS-BoldMT", size: 17, lineHeight: 22, textStyle: .headline)
+    ),
+    body: Body(
+      body: BTFont(fontName: "TimesNewRomanPSMT", size: 17, lineHeight: 22, textStyle: .body),
+      bodyBold: BTFont(fontName: "TimesNewRomanPS-BoldMT", size: 17, lineHeight: 22, textStyle: .body),
+      subheadline: BTFont(fontName: "TimesNewRomanPSMT", size: 15, lineHeight: 20, textStyle: .subheadline),
+      subheadlineBold: BTFont(fontName: "TimesNewRomanPS-BoldMT", size: 15, lineHeight: 20, textStyle: .subheadline),
+      footnote: BTFont(fontName: "TimesNewRomanPSMT", size: 13, lineHeight: 18, textStyle: .footnote),
+      footnoteBold: BTFont(fontName: "TimesNewRomanPS-BoldMT", size: 13, lineHeight: 18, textStyle: .footnote)
+    ),
+    caption: Caption(
+      caption1: BTFont(fontName: "TimesNewRomanPSMT", size: 12, lineHeight: 16, textStyle: .caption1),
+      caption1Bold: BTFont(fontName: "TimesNewRomanPS-BoldMT", size: 12, lineHeight: 16, textStyle: .caption1),
+      caption2: BTFont(fontName: "TimesNewRomanPSMT", size: 11, lineHeight: 13, textStyle: .caption2),
+      caption2Bold: BTFont(fontName: "TimesNewRomanPS-BoldMT", size: 11, lineHeight: 13, textStyle: .caption2)
+    )
+  )
+}
+
+// MARK: - Spacing Definition
+
+extension Spacing {
+  static let mainSpacing = Spacing(
+    lineWidth: 1.0,
+    spacerXS: 4.0,
+    spacerS: 8.0,
+    spacerM: 12.0,
+    spacerL: 16.0,
+    spacerXL: 20.0,
+    spacerXXL: 24.0
+  )
+}
---
+++ Modules/CoreUI/CoreUI/Sources/Utilities/BTFont.swift
@@ -0,0 +1,68 @@
+//
+//  BTFont.swift
+//  CoreUI
+//
+//  Created by Tarciziu Gologan on 31.08.2025.
+//
+
+import SwiftUI
+import UIKit
+
+/// A custom font with a specific line height.
+/// This struct encapsulates a `UIFont` and its associated line height,
+/// allowing for consistent text styling throughout the app.
+public struct BTFont {
+  public let font: UIFont
+  let lineHeight: CGFloat
+
+  public init(fontName: String, size: CGFloat, lineHeight: CGFloat, textStyle: UIFont.TextStyle) {
+    let customFont = UIFont(name: fontName, size: size) ?? UIFont.systemFont(ofSize: size)
+    self.font = UIFontMetrics(forTextStyle: textStyle).scaledFont(for: customFont)
+    self.lineHeight = lineHeight
+  }
+}
+
+// MARK: - Font with line height modifier
+
+/// Solution found here: https://stackoverflow.com/questions/61705184/how-to-set-line-height-for-a-single-line-text-in-swiftui.
+struct FontWithLineHeight: ViewModifier {
+  let btFont: BTFont
+
+  func body(content: Content) -> some View {
+    let heightDifference = btFont.lineHeight - btFont.font.lineHeight
+    let spacing: CGFloat = max(heightDifference, .zero)
+    let padding = max(heightDifference / 2, .zero)
+    return content
+      .font(Font(btFont.font))
+      .padding(.vertical, padding)
+      .lineSpacing(spacing)
+  }
+}
+
+extension Text {
+  /// Applies a custom font with a specific line height to the text.
+  /// - Parameter btFont: The custom font and line height to apply.
+  /// - Returns: A text view with the specified font and line height.
+  public func font(_ btFont: BTFont) -> some View {
+    ModifiedContent(content: self, modifier: FontWithLineHeight(btFont: btFont))
+  }
+}
+
+// MARK: - BTFont applier
+
+struct BTFontApplier: ViewModifier {
+  let btFont: BTFont
+
+  func body(content: Content) -> some View {
+    content.font(btFont)
+  }
+}
+
+extension View {
+  /// Applies a custom font with a specific line height to the view.
+  /// - Parameter btFont: The custom font and line height to apply.
+  /// - Returns: A view with the specified font and line height.
+  public func font(_ btFont: BTFont) -> some View {
+    ModifiedContent(content: self, modifier: BTFontApplier(btFont: btFont))
+  }
+}
