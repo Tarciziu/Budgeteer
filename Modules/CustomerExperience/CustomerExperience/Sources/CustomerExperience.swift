7f6770fab415ce2166461c6e9b49ff0528034e9e
AdrianHz <herczegadrianz@gmail.com>
2025-09-30 14:34:42 +0300

    Add customer experience module
    Ticket: 14h

---
+++ Modules/CustomerExperience/CustomerExperience/Sources/CustomerExperience.swift
@@ -0,0 +1,8 @@
+//
+//  CustomerExperience.swift
+//  CustomerExperience
+//
+//  Created by Adrian-Zoltan Herczeg on 30.09.2025.
+//
+
+import Foundation
---
+++ Modules/CustomerExperience/CustomerExperienceTests/CustomerExperienceTests.swift
@@ -0,0 +1,16 @@
+//
+//  CustomerExperienceTests.swift
+//  CustomerExperienceTests
+//
+//  Created by Adrian-Zoltan Herczeg on 30.09.2025.
+//
+
+import Testing
+@testable import CustomerExperience
+
+struct CustomerExperienceTests {
+  @Test
+  func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+}
