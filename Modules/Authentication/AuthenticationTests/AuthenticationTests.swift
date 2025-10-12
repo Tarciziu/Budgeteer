d7122610abc3b3e22c134e773bf8a3dac013bc7c
Tarciziu <christin99tarciziu@gmail.com>
2025-10-12 15:18:32 +0300

    Add Authentication module
    - Added BTAuthentication dependency to main module
    
    Ticket: $16h
    
    Type: New Feature

---
+++ Modules/Authentication/AuthenticationTests/AuthenticationTests.swift
@@ -0,0 +1,16 @@
+//
+//  AuthenticationTests.swift
+//  AuthenticationTests
+//
+//  Created by Tarciziu Gologan on 12.10.2025.
+//
+
+import Testing
+@testable import BTAuthentication
+
+struct AuthenticationTests {
+  @Test
+  func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+}
