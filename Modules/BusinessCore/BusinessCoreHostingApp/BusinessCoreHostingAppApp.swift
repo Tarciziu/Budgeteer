3c76cd035a9dab2a3a2f11688499e64e72f4a721
AdrianHz <herczegadrianz@gmail.com>
2026-01-07 17:37:59 +0200

    Implement BusinessCore module
    - Create BusinessCore module with basic structure
    - Update project and workspace to include new module
    
    Ticket: 17c
    
    Type: New Feature

---
+++ Modules/BusinessCore/BusinessCoreHostingApp/BusinessCoreHostingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCoreHostingAppApp.swift
+//  BusinessCoreHostingApp
+//
+//  Created by Adrian-Zoltan Herczeg on 07.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCoreHostingAppApp: App {
+  var body: some Scene {
+    WindowGroup {
+      Text("Hello, world!")
+    }
+  }
+}
---
+++ Modules/BusinessCore/BusinessCoreTests/BusinessCoreTests.swift
@@ -0,0 +1,16 @@
+//
+//  BusinessCoreTests.swift
+//  BusinessCoreTests
+//
+//  Created by Adrian-Zoltan Herczeg on 07.01.2026.
+//
+
+import Testing
+@testable import BusinessCoreHostingApp
+
+struct BusinessCoreTests {
+  @Test
+  func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+}
